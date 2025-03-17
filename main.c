a7e9a9b96de43980a1b9ff961d87d47f06a82007
Lukas Henningsson <kasaklo.henningsson@gmail.com>
2025-03-17 15:00:58 +0100

    Initial project with a basic map and tile definitions

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "raylib.h"
 
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+#define CHUNK_SIZE 32
+#define MAP_WIDTH 32
+#define MAP_HEIGHT 32
+
+typedef enum {
+    TILE_EMPTY,
+    TILE_PATH,
+    TILE_TOWER_SPOT,
+    TILE_CASTLE,
+    TILE_SPAWN
+} TileType;
+
+TileType map[MAP_WIDTH][MAP_HEIGHT];
+
+
+int main() {
+    InitWindow(1024, 1024, "Spire");
+    SetTargetFPS(60);
+
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    EndDrawing();
 }
